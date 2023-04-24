2e989c6f1cff931bbe311888e84d377525f7a3ac
Cormen19 <76107484+Cormen19@users.noreply.github.com>
2023-04-24 19:23:52 +0200

    a

---
+++ EcoMapsServer/Data/ContNaranja.cs
@@ -0,0 +1,15 @@
+namespace EcoMapsServer.Data
+{
+    public class ContNaranja
+    {
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public double? Longitude { get; set; }
+
+        public double? Latitude { get; set; }
+
+        public string? WKT { get; set; }
+    }
+}
---
+++ EcoMapsServer/Data/PuntosLimpio.cs
@@ -0,0 +1,23 @@
+namespace EcoMapsServer.Data
+{
+    public class PuntosLimpio
+    {
+    
+
+        public string? Name { get; set; }
+
+        public double? Latitude{ get; set; }
+
+        public double? Longitude { get; set; }
+
+        public int? Telefono { get; set; }
+
+        public string? Provincia { get; set; }
+
+        public string? Concello { get; set; }
+
+        public string? WKT{get;set;}
+
+        
+    }
+}
