4c337bf7be5fdedd8974cffc1e42b4e5ef511800
Ekaitz Cormenzana <76107484+Cormen19@users.noreply.github.com>
2023-05-17 17:03:22 +0200

    a

---
+++ EcoMapsServer/Endpoints.cs
@@ -0,0 +1,10 @@
+﻿namespace EcoMapsServer
+{
+    public class Endpoints
+    {
+        public static string GetAllPersonajes()
+        {
+            return $"https://rickandmortyapi.com/api/character";
+        }
+    }
+}
