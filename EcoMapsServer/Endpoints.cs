b94511b17a0ee65bbd42ea0c689b721398f6f31f
Ekaitz Cormenzana <76107484+Cormen19@users.noreply.github.com>
2023-05-18 16:54:31 +0200

    a

--- EcoMapsServer/Endpoints.cs
+++ EcoMapsServer/Endpoints.cs
@@ -6,5 +6,13 @@
         {
             return $"https://rickandmortyapi.com/api/character";
         }
+        public static string GetPersonajesId(string id)
+        {
+            return $"https://rickandmortyapi.com/api/character/{id}";
+        }
+        public static string GetPersonajesByName(string name)
+        {
+            return $"https://rickandmortyapi.com/api/character/?name={name}";
+        }
     }
 }
---
+++ EcoMapsServer/Model/PersonajeUnico.cs
@@ -0,0 +1,32 @@
+﻿namespace EcoMapsServer.Model
+{
+
+    public class PersonajeUnico
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+        public string species { get; set; }
+        public string type { get; set; }
+        public string gender { get; set; }
+        public Origin1 origin { get; set; }
+        public Location1 location { get; set; }
+        public string image { get; set; }
+        public string[] episode { get; set; }
+        public string url { get; set; }
+        public DateTime created { get; set; }
+    }
+
+    public class Origin1
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Location1
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+}
